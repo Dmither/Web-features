cb281e5cce7cc56c09b631d539f1e3ef25c49559
Dmither <mr.d.graumann@gmail.com>
2022-09-14 16:26:24 +0300

    Change form elements

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ function toggleMenu(){
   document.querySelector('.menu-mobile').classList.toggle('menu_open');
   document.querySelector('.burger').classList.toggle('burger_open');
 }
+
+document.querySelectorAll('.checkbox').forEach(function(index){
+  index.addEventListener('click', function(e){
+    console.log(e.target);
+  })
+})
+

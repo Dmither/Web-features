ced3540218383d189dbe59a12734de553941ab37
Dmither <mr.d.graumann@gmail.com>
2022-10-07 13:58:23 +0300

    Add slider

--- js/script.js
+++ js/script.js
@@ -36,7 +36,6 @@ document.querySelectorAll('.radio').forEach(function(elem){
 })
 }
 
-
 // spoilers
 {
 var spoilers = document.querySelectorAll('.spoiler');
@@ -60,85 +59,6 @@ document.querySelectorAll('.spoiler__title').forEach(function(elem){
       spoiler.querySelector('.spoiler__text').style.height = `${textHeight + 2}px`;
     } else {spoiler.querySelector('.spoiler__text').style.height = '0px';}
   })
-})
-}
-
-// slider
-{
-// get and add start elements classes
-var sliderBox = document.querySelector('.slider__box');
-var sliderItems = document.querySelectorAll('.slider__image');
-var current = document.querySelector('.slider__image_current');
-var currentIndex = Array.prototype.indexOf.call(sliderItems, current);
-if (currentIndex <= 0){
-  var prevIndex = sliderItems.length - 1;
-  var nextIndex = currentIndex + 1;
-} else if (currentIndex >= sliderItems.length - 1){
-  var prevIndex = currentIndex - 1;
-  var nextIndex = 0;
-}
-else {
-  var prevIndex = currentIndex - 1;
-  var nextIndex = currentIndex + 1;
-}
-var prevElement = sliderBox.children[prevIndex];
-var nextElement = sliderBox.children[nextIndex];
-prevElement.classList.add('slider__image_prev');
-nextElement.classList.add('slider__image_next');
-// add prev and next buttons logic
-var prev = document.querySelector('.slider__prev')
-prev.addEventListener('click', function(event){
-  if (currentIndex <= 0){
-    currentIndex = sliderItems.length - 1;
-  } else {
-    currentIndex--;
-  } 
-  if (prevIndex <= 0){
-    prevIndex = sliderItems.length - 1;
-  } else {
-    prevIndex--;
-  } 
-  if (nextIndex <= 0){
-    nextIndex = sliderItems.length - 1;
-  } else {
-    nextIndex--;
-  } 
-  current.classList.remove('slider__image_current');
-  current = sliderBox.children[currentIndex];
-  current.classList.add('slider__image_current');
-  prevElement.classList.remove('slider__image_prev');
-  prevElement = sliderBox.children[prevIndex];
-  prevElement.classList.add('slider__image_prev');
-  nextElement.classList.remove('slider__image_next');
-  nextElement = sliderBox.children[nextIndex];
-  nextElement.classList.add('slider__image_next');
-})
-var next = document.querySelector('.slider__next');
-next.addEventListener('click', function(event){
-  if (currentIndex >= sliderItems.length - 1){
-    currentIndex = 0;
-  } else {
-    currentIndex++;
-  } 
-  if (prevIndex >= sliderItems.length - 1){
-    prevIndex = 0;
-  } else {
-    prevIndex++;
-  } 
-  if (nextIndex >= sliderItems.length - 1){
-    nextIndex = 0;
-  } else {
-    nextIndex++;
-  } 
-  current.classList.remove('slider__image_current');
-  current = sliderBox.children[currentIndex];
-  current.classList.add('slider__image_current');
-  prevElement.classList.remove('slider__image_prev');
-  prevElement = sliderBox.children[prevIndex];
-  prevElement.classList.add('slider__image_prev');
-  nextElement.classList.remove('slider__image_next');
-  nextElement = sliderBox.children[nextIndex];
-  nextElement.classList.add('slider__image_next');
 })
 }
 
@@ -171,3 +91,32 @@ next.addEventListener('click', function(event){
     }
   }
 }
+
+new Swiper('.slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: false,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: true,
+    disableOnInteraction: false,
+  },
+  speed: 1000,
+  thumbs: {
+    swiper: {
+      el: '.slider-mini',
+      slidesPerView: 4,
+    }
+  }
+});
